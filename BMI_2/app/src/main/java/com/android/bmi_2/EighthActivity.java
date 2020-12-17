20c0695479e4b4179f7c0780ec5f1623079c37e9
DaehwanKIm <e994301s@gmail.com>
2020-12-17 17:25:59 +0900

    history

--- BMI_2/app/src/main/java/com/android/bmi_2/EighthActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/EighthActivity.java
@@ -2,12 +2,26 @@ package com.android.bmi_2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class EighthActivity extends Activity {
+
+    private ArrayList<History> data = null;
+    private HistoryAdapter adapter = null;
+    private ListView listView = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eighth);
+
+        data = new ArrayList<History>();
+        adapter = new HistoryAdapter(EighthActivity.this, R.layout.eighth_layout, data);
+        listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
