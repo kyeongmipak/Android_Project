d012d7324945fb3a2bc7f4eb389fe1f7f482db1f
RiaSong <donna_s@sookmyung.ac.kr>
2020-12-17 18:15:54 +0900

    12/17 Ria

--- BMI_2/app/src/main/java/com/android/bmi_2/FifthActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/FifthActivity.java
@@ -1,14 +1,43 @@
 package com.android.bmi_2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
 
 import androidx.annotation.Nullable;
 
 public class FifthActivity extends Activity {
 
+    RatingBar ratingBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth);
+
+        findViewById(R.id.ratingBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ratingBar = findViewById(R.id.rating);
+//                String rating = getText(ratingBar).toString();
+
+                new AlertDialog.Builder(FifthActivity.this)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setTitle("* 별점 등록 완료! *")
+                        .setMessage("감사합니다! 😍")
+                        // 버튼명   // 버튼 리스너명
+                        .setPositiveButton("닫기", null)
+                        .show();
+            }
+        });
+
+        findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- BMI_2/app/src/main/java/com/android/bmi_2/SecondActivity.java
+++ BMI_2/app/src/main/java/com/android/bmi_2/SecondActivity.java
@@ -1,15 +1,69 @@
 package com.android.bmi_2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 
 public class SecondActivity extends Activity {
+
+    Button backBtn, nextBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
 
-    }
-}
+        ImageView female = findViewById(R.id.female);
+
+        Switch sw = (Switch)findViewById(R.id.selectGender);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public
+            void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                // TODO Auto-generated method stub
+
+                if(sw.isChecked()){
+                    female.setImageResource(R.drawable.female);
+                } else {
+                    female.setImageResource(R.drawable.male);
+                }
+            }
+        });
+
+        findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 화면 넘길때는 intent가 필요하다! ,를 기준으로 from & to 지정해줘야 함.
+                // MainActivity 에서 SubActivity 로 보내겠다!
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.nextBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 화면 넘길때는 intent가 필요하다! ,를 기준으로 from & to 지정해줘야 함.
+                // MainActivity 에서 SubActivity 로 보내겠다!
+                 Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                if (sw.isChecked()) {
+                    intent.putExtra("gender", "female");
+                } else{
+                    intent.putExtra("gender", "male");
+                }
+                 startActivity(intent);
+            }
+        });
+    } // onCreate End
+
+
+} // end
